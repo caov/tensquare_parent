10e21af2e1de34569781cf372bef0c954810ad72
caov <caovan@163.com>
2019-06-11 17:44:39 +0800

    add

---
+++ tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -0,0 +1,11 @@
+package com.tensquare.base.controller;
+
+/**
+ * @program: tensquare_parent
+ * @description:标签控制类
+ * @author: cf
+ * @create: 2019-06-11 17:16
+ */
+public class LabelController {
+
+}
---
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -0,0 +1,10 @@
+package com.tensquare.base.service;
+
+/**
+ * @program: tensquare_parent
+ * @description: 标签Service类
+ * @author: cf
+ * @create: 2019-06-11 17:18
+ */
+public class LabelService {
+}
